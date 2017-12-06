d674d590427171d21ce3eb36ea4bb4f484c93855
Unknown <ghifarozarahmadiana@yahoo.com>
2017-12-06 22:55:10 +0700

    done. (masih perlu dicek)

--- cd.c
+++ cd.c
@@ -11,4 +11,15 @@ int main (int argc, char *argv[])
 		printf (1, "format penggunaan: cd [direktori yang ingin dituju]\n");
 		exit();
 	}
+
+	int temp=0;
+	temp = chdir(argv[1]);
+
+	if (temp < 0)
+	{
+		printf (1, "tidak bisa pindah directory\n");
+		exit();
+	}
+	printf (1, "berhasil pindah directory ke %s\n", argv[1]);
+	exit();
 }
