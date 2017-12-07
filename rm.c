9d2f88100946566c1315d441ae7a58702fc3034e
John Harison <john.harison99@gmail.com>
2017-12-08 00:15:10 +0700

    terkadang cinta itu buta. ayo kita coba

---
+++ rm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <malloc.h>
+#include <string.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fnctl.h>
+#include <unistd.h>
+int main(int argc, char *argv[]){
+	unlink(argv[1]);
+}
