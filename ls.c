1a1ece2fe591b282cc9b6e6eaebfb285693198b2
yoshimaputri <yoshimashima@gmail.com>
2017-12-08 12:03:17 +0700

    ls modified

--- ls.c
+++ ls.c
@@ -2,23 +2,32 @@
 #include <syscall.h>
 #include <string.h>
 #include <stat.h>
+#include <dirent.h>
 
 char* fname(char *path)
 {
-    
+    static char buf[charMAX];
+    char *p;
+    for(p=path+strlen(path); p >= path && *p != '/'; p--) //ngambil karakter prtama setelah '/'
+        ;p++;
+    if(strlen(p) >= charMAX)
+        return p;
+    memmove(buf, p, strlen(p)); //copy nama file
+    memset(buf+strlen(p), ' ', charMAX-strlen(p));
+    return buf;
 }
 
 void ls(char *path)
 {
     char buf[512]; int fp; char *tmp;
-    struct stat jfile; struct dirent die;
+    struct stat st; struct dirent die;
     //printf 0: stdin, 1: stdout, 2:stderr
     if((fp=open(path,0)) < 0) { printf(2, "[ERR] gabisa buka file %s\n", path); return;}
     if(stat(fp,&st) < 0){
         printf(2, "[ERR] %s bukan file, dir, maupun device\n", path); 
         close(fp); return;}
 
-    switch(jfile.type){
+    switch(st.type){
         case 1: //kalo dir
             if(strlen(path) + 1 + charMAX + 1 > sizeof buf){ //kalo path <20
             printf(1, "path lebih dari 20\n");
@@ -26,17 +35,20 @@ void ls(char *path)
             strcpy(buf, path);
             tmp = buf+strlen(buf);
             *tmp++ = '/';
-            while(read(fp, &die, sizeof(die)) == sizeof(die)){
+            while(read(fp, &die, sizeof(die)) == sizeof(die)){ //dia bakal nge loop dari dir itu, ngecek kalo file di dlmnya mrupakan st.type yg sesuai
                 if(die.inum == 0) continue;
                 memmove(tmp, die.name, charMAX);
                 tmp[charMAX] = 0;
                 if(stat(buf, &st) < 0){
                     printf(1, "[ERR] %s bukan file, dir, maupun device\n", buf); continue;}
-                printf(1, "%s %d %d %d\n", fname(buf), st.type, st.ino, st.size);
+                printf(1, "%s %d %d %d\n", fname(buf), st.type, st.ino, st.size); //nampilin nama filenya, tipe file *stat*, inode, size of file
             }
             break;
         case 2: //kalo file
+            printf(1, "%s %d %d %d\n", fname(path), st.type, st.ino, st.size);
+            break;
     }
+    close(fp);
 }
 
 int main(int argc, char *argv[])
