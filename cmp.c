6afd2143943c04f1abb4e03d82be933046d27fc8
Unknown <ghifarozarahmadiana@yahoo.com>
2017-12-08 22:32:13 +0700

    Auto stash before merge of "master" and "origin/master"
    inisial

---
+++ cmp.c
@@ -0,0 +1,15 @@
+#include"types.h"
+#include"stat.h"
+#include"user.h"
+#include"fcntl.h"
+#include"fs.h"
+
+int main(int argc, char* argv[])
+{
+    if(argc != 3)
+    {
+       printf (1, "format penggunaan: cmp [nama file1] [nama file2]\n");
+        exit();
+    }
+    exit();
+}
