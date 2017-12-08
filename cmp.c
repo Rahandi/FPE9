d8abb64a5b9fcc298898a31108fc4b1f81a3a9ea
Unknown <ghifarozarahmadiana@yahoo.com>
2017-12-09 06:15:38 +0700

    fix. done

--- cmp.c
+++ cmp.c
@@ -35,7 +35,6 @@ int main (int argc, char *argv[])
 		line = 1;
 		while (1)
 		{
-			byte++;
         		if((read(fd1, &t1, sizeof(t1))) <= 0 || (read(fd2, &t2, sizeof(t2))) <= 0)
 			{
 				break;
@@ -48,6 +47,7 @@ int main (int argc, char *argv[])
 			{
             			break;
         		}
+			++byte;
 		}
 
 		if(read(fd1, &t1, sizeof(t1))!=0 || read(fd2, &t2, sizeof(t2))!=0)
