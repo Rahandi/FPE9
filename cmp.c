d57671f70d6ae3db1c4f14d3ee6933343108a2f7
Unknown <ghifarozarahmadiana@yahoo.com>
2017-12-08 22:50:37 +0700

    tambah sedikit

--- cmp.c
+++ cmp.c
@@ -4,12 +4,30 @@
 #include"fcntl.h"
 #include"fs.h"
 
-int main(int argc, char* argv[])
+int main (int argc, char *argv[])
 {
-    if(argc != 3)
-    {
-       printf (1, "format penggunaan: cmp [nama file1] [nama file2]\n");
-        exit();
-    }
-    exit();
+    	if(argc != 3)
+    	{
+		printf (1, "format penggunaan: cmp [nama file1] [nama file2]\n");
+        	exit();
+    	}
+    	exit();
+    	
+	int fd1, fd2;
+	if((fd1 = open(argv[1], O_RDONLY) < 0)
+    	{
+      		printf(1, "%s tidak ditemukan\n", argv[1]);
+        	exit();
+    	}
+
+    	else if((fd2 = open(argv[2], O_RDONLY) < 0)
+    	{	
+        	printf(1, "%s tidak ditemukan\n", argv[2]);
+        	exit();
+    	}
+	
+	else
+	{
+		
+	}
 }
