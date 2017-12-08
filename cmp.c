ddd461a6c7abd57afe6973c7ef4522b87fa5568a
Unknown <ghifarozarahmadiana@yahoo.com>
2017-12-09 05:47:17 +0700

    tambah untuk compare

--- cmp.c
+++ cmp.c
@@ -6,20 +6,19 @@
 
 int main (int argc, char *argv[])
 {
+	int fd1, fd2, byte, t1, t2, line;
     	if(argc != 3)
     	{
 		printf (1, "format penggunaan: cmp [nama file1] [nama file2]\n");
         	exit();
     	}
-    	
-	int fd1, fd2;
-	if((fd1 = open(argv[1], O_RDONLY) < 0)
+	if((fd1 = open(argv[1], O_RDONLY)) < 0)
     	{
       		printf(1, "%s tidak ditemukan\n", argv[1]);
         	exit();
     	}
 
-    	else if((fd2 = open(argv[2], O_RDONLY) < 0)
+    	else if((fd2 = open(argv[2], O_RDONLY)) < 0)
     	{	
         	printf(1, "%s tidak ditemukan\n", argv[2]);
         	exit();
@@ -27,6 +26,58 @@ int main (int argc, char *argv[])
 	
 	else
 	{
-		
+		byte = 0;
+		line = 1;
+		while (1)
+		{
+        		if((read(fd1, &t1, sizeof(t1))) <= 0 || (read(fd2, &t2, sizeof(t2))) <= 0)
+			{
+				break;
+			}
+			if(t1 == '\n' && t2 == '\n')
+			{
+				++line;
+			}
+        		if(t1 != t2)
+			{
+            			break;
+        		}
+		}
+		if(read(fd1, &t1, sizeof(t1))!=0 || read(fd2, &t2, sizeof(t2))!=0)
+    		{
+	 		printf(1, "file %s dan file %s berbeda pada: byte %d line %d\n", argv[1], argv[2], byte, line);
+    		}
+		else
+		{
+			printf(1, "tidak ada perbedaan\n");
+		}
+		close(fd1);
+    		close(fd2);
 	}
+	exit();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
